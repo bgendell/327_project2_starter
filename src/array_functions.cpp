53878bede5494dda63c27d8b968ef3307aa8b921
Ben G <benjamin.gendell.16@cnu.edu>
2019-09-23 11:56:18 -0400

    add definitions and get initial compile

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -1,12 +1,23 @@
 /*
- * functionstocomplete.cpp
+ * array_functions.cpp
  *
- *  Created on: Sep 10, 2017
- *      Author: keith
+ *  Created on: Sep 16, 2019
+ *      From: keith
+ *      Author: Ben Gendell
  */
 
 //============================================================================
 //	TODO add necessary includes here
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstring>
+
+#include "array_functions.h"
+#include "utilities.h"
+#include "constants.h"
+
 //============================================================================
 
 //============================================================================
@@ -21,3 +32,64 @@
 //TODO define all functions in header file
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
+struct entries{
+	std::string word;
+	int num_occur;
+};
+void clearArray(){
+
+}
+
+int getArraySize(){
+	return constants::SUCCESS;
+}
+
+std::string getArrayWordAt(int i){
+	std::string testReturn = "return";
+	return testReturn;
+}
+
+int getArrayWord_NumbOccur_At(int i){
+	return i;
+}
+
+bool processFile(std::fstream &myfstream){
+	return false;
+}
+
+void processLine(std::string &myString){
+
+}
+
+void processToken(std::string &token){
+
+}
+
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+		std::ios_base::openmode mode){
+	//open file
+	myfile.open(myFileName.c_str(),mode);
+
+	//check for returns
+	if(myfile.is_open()){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void closeFile(std::fstream& myfile){
+	if (myfile.is_open()){
+		myfile.close();
+	}
+
+}
+
+int writeArraytoFile(const std::string &outputfilename){
+	return constants::SUCCESS;
+}
+
+void sortArray(constants::sortOrder so){
+
+}
