8898279824b919859ce9190de6b2ed9a240d38db
Ben G <benjamin.gendell.16@cnu.edu>
2019-09-24 21:32:41 -0400

    score of 45; passes tests through processLine

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -36,37 +36,85 @@ struct entries{
 	std::string word;
 	int num_occur;
 };
-void clearArray(){
+entries list[constants::MAX_WORDS];
+int listSize = 0;
 
+void clearArray(){
+	for(int i=0;i<=abs(sizeof(&list));i++){
+		list[i].word = "";
+		list[i].num_occur = 0;
+	}
+	listSize = 0;
 }
 
 int getArraySize(){
-	return constants::SUCCESS;
+	while (list[listSize].word != ""){
+		listSize++;
+	}
+	return listSize;
 }
 
 std::string getArrayWordAt(int i){
-	std::string testReturn = "return";
-	return testReturn;
+	return list[i].word;
 }
 
 int getArrayWord_NumbOccur_At(int i){
-	return i;
+	return list[i].num_occur;
 }
 
+/*loop through whole file, one line at a time
+ * call processLine on each line
+ * returns false: myfstream is not open
+ *         true: otherwise*/
 bool processFile(std::fstream &myfstream){
-	return false;
+	if(!myfstream.is_open()){
+		return false;
+	}
+	else{
+		std::string line;
+		while(!myfstream.eof()){
+			getline(myfstream,line);
+			processLine(line);
+		}
+		return true;
+	}
 }
-
+/*take 1 line and extract all the tokens from it
+feed each token to processToken for recording*/
 void processLine(std::string &myString){
-
+	if(myString == ""){
+		processToken(myString);
+	}
+	std::stringstream ss(myString);
+	std::string temp;
+	while(getline(ss,temp,constants::CHAR_TO_SEARCH_FOR)){
+		processToken(temp);
+	}
 }
 
 void processToken(std::string &token){
+	strip_unwanted_chars(token);
+	if(token==""){
+		return;
+	}
+	//toUpper(token);
 
+	for(int i=0;i<listSize;i++){
+		std::string a = token;
+		std::string b = list[i].word;
+		toUpper(a);
+		toUpper(b);
+		if(a == b){
+			list[i].num_occur++;
+			return;
+		}
+	}
+	list[listSize].word = token;
+	list[listSize].num_occur = 1;
+	listSize++;
 }
 
-bool openFile(std::fstream& myfile, const std::string& myFileName,
-		std::ios_base::openmode mode){
+bool openFile(std::fstream& myfile, const std::string& myFileName, std::ios_base::openmode mode){
 	//open file
 	myfile.open(myFileName.c_str(),mode);
 
@@ -86,10 +134,69 @@ void closeFile(std::fstream& myfile){
 
 }
 
+/* serializes all content in myEntryArray to file outputfilename
+ * returns  FAIL_FILE_DID_NOT_OPEN if cannot open outputfilename
+ * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
+ * 			SUCCESS if all data is written and outputfilename closes OK
+ * */
 int writeArraytoFile(const std::string &outputfilename){
-	return constants::SUCCESS;
+	std::fstream fileToOut;
+	bool test = openFile(fileToOut,outputfilename,std::ios_base::out);
+	if (!test){
+		return constants::FAIL_FILE_DID_NOT_OPEN;
+	}
+	if (listSize == 0){
+			return constants::FAIL_NO_ARRAY_DATA;
+		}
+	else{
+		for (int i=0;i<listSize;i++){
+					fileToOut << list[i].word;
+					fileToOut << " ";
+					fileToOut << list[i].num_occur;
+					fileToOut <<"\n";
+				}
+				closeFile(fileToOut);
+		return constants::SUCCESS;
+	}
+
 }
 
 void sortArray(constants::sortOrder so){
+	if(so == constants::NONE){
+		return;
+	}
+	if(so==constants::ASCENDING){
+		for(int j = 0;j<listSize;j++){
+			for (int i=0;i<listSize-1;i++){
+				if(list[i+1].word < list[i].word){
+					std::string holder =  list[i+1].word;
+					list[i+1].word = list[i].word;
+					list[i].word = holder;
+				}
+			}
+		}
+	}
+	if(so==constants::DESCENDING){
+		for(int j = 0;j<listSize;j++){
+			for (int i=0;i<listSize-1;i++){
+				if(list[i+1].word > list[i].word){
+					std::string holder =  list[i+1].word;
+					list[i+1].word = list[i].word;
+					list[i].word = holder;
+				}
+			}
+		}
+	}
+	if(so==constants::NUMBER_OCCURRENCES){
+		for(int j = 0;j<listSize;j++){
+			for (int i=0;i<listSize-1;i++){
+				if(list[i+1].num_occur > list[i].num_occur){
+					int holder =  list[i+1].num_occur;
+					list[i+1].num_occur = list[i].num_occur;
+					list[i].num_occur = holder;
+				}
+			}
+		}
+	}
 
 }
